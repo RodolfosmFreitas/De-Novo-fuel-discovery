1a2d8e62994851819b7ca7d0ebb23c600c4d763d
RodolfosmFreitas <142793854+RodolfosmFreitas@users.noreply.github.com>
2025-07-29 16:17:10 +0100

    Update pos-processing.py

--- Virtual-screening/pos-processing.py
+++ Virtual-screening/pos-processing.py
@@ -47,7 +47,7 @@ def predict(test_x):
     f_samples = Y_pred.sample(sample_shape=torch.Size([1000])).detach().cpu().numpy() * std_ + mu_ 
     
     mu_pred = f_samples.mean(0) 
-    var_pred = Y_pred.var(0)
+    var_pred = f_samples.var(0)
     sigma_pred = np.sqrt(var_pred)
     return mu_pred, sigma_pred
 
